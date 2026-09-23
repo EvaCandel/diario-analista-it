a8d7657957ab1330d6fcb1b87b689f8a2a6b02dc
Eva <evacp888@gmail.com>
2026-09-23 19:43:22 +0200

    feat: implementacion de listas y bucle for en monitor de red

--- python_scripts/monitor.py
+++ python_scripts/monitor.py
@@ -1,13 +1,20 @@
 import subprocess
 
-print("=== Monitor de Red Python ===")
-objetivo = "10.200.200.200"
+print("=== Monitor de Red Python Múltiple ===")
+# 1. Definimos una lista con 3 IPs. La última es inventada para forzar un fallo.
+lista_ips = ["8.8.8.8", "1.1.1.1", "10.200.200.200"]
 
-# Ejecutamos un ping de 1 paquete. capture_output oculta el texto técnico de Linux.
-resultado = subprocess.run(["ping", "-c", "1", objetivo], capture_output=True)
-
-# El código de retorno 0 en Linux significa que el comando funcionó sin errores.
-if resultado.returncode == 0:
-    print(f"[+] ALERTA: El equipo {objetivo} está RESPONDIENDO.")
-else:
-    print(f"[-] AVISO: El equipo {objetivo} parece estar CAÍDO.")
+# 2. El bucle for pasará por cada IP de la lista, una por una.
+for ip_actual in lista_ips:
+    print(f"[*] Analizando {ip_actual}...")
+    
+    # 3. La variable objetivo ahora cambia dinámicamente en cada vuelta
+    resultado = subprocess.run(["ping", "-c", "1", ip_actual], capture_output=True)
+    
+    # 4. Evaluamos el resultado
+    if resultado.returncode == 0:
+        print(f"    [+] ALERTA: El equipo {ip_actual} RESPONDE.")
+    else:
+        print(f"    [-] AVISO: El equipo {ip_actual} NO RESPONDE.")
+        
+print("=== Escaneo Finalizado ===")
